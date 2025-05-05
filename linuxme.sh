f350278027738ed62af37409c5f1a2d121e943fe
Johannes <github.finch886@passmail.net>
2025-05-05 14:51:42 +0200

    changed linux script and gitignore

--- linuxme.sh
+++ linuxme.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+wget=/usr/bin/wget
+
+CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+URL_BASE="https://download.mono-project.com/sources/mono"
+WGET_OPTS=''
+VERSION="6.12.0.199"
+TARGET_MONO_DIR_NAME="mono-$VERSION"
+REMOTE_FILENAME="mono-$VERSION.tar.xz"
 PREFIX=$@
 if [ -z $PREFIX ]; then
-  PREFIX="/home/johannes/Playground/cppwithcsharp/momo-heaven"
+  PREFIX="$CURRENT_DIR/momo"
 fi
 
 # Ensure you have write permissions to PREFIX
@@ -16,19 +24,36 @@ fi
 sudo chown -R `whoami` $PREFIX
 
 # Ensure that all required packages are installed.
-sudo apt-get install git autoconf libtool automake build-essential gettext cmake python libkrb5-dev libatomic-ops-dev zlib1g-dev
+sudo apt-get install git autoconf libtool libtool-bin automake build-essential gettext cmake python python-is-python3 2to3 libkrb5-dev libatomic-ops-dev zlib1g-dev
 
 PATH=$PREFIX/bin:$PATH
 
-if [ -d momomain ]; then
-  echo "momomain directory exists."
+if [ -d $TARGET_MONO_DIR_NAME ]; then
+  echo "$TARGET_MONO_DIR_NAME directory already exists."
+  exit 1
 else
-  git clone https://github.com/mono/mono.git momomain
+  if [ -f $REMOTE_FILENAME ]; then
+    echo "$REMOTE_FILENAME exists already!"
+  else
+    if ! $wget $WGET_OPTS "$URL_BASE/$REMOTE_FILENAME"; then
+      echo "ERROR: can't get archive $VERSION" >&2
+      exit 1
+    fi
+  fi
 fi
 
-cd momomain
+if [ -d $TARGET_MONO_DIR_NAME ]; then
+  echo "$TARGET_MONO_DIR_NAME directory already exists."
+else
+  if [ -f $REMOTE_FILENAME ]; then
+    tar xvf $REMOTE_FILENAME
+  else
+    echo "ERROR: $REMOTE_FILENAME is missing!"
+    exit 1
+  fi
+fi
 
-git checkout mono-6.12.0.200
+cd $TARGET_MONO_DIR_NAME
 
 ./autogen.sh --prefix=$PREFIX
 
